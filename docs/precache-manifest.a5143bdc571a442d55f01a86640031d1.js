f73a40e443262b50550049f5dc67cbf6364b83bf
igarciavalcarcel <igarciavalcarcel@gmail.com>
2021-03-02 17:51:38 +0100

    docs agregado

---
+++ docs/precache-manifest.a5143bdc571a442d55f01a86640031d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df02c7ed3860e1ce4cd8f3307116211",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e01ade3f24f069d5a0b8",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e01ade3f24f069d5a0b8",
+    "url": "/static/js/main.df29c38e.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
